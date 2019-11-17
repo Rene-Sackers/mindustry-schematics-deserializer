0fecebdee9fc2e2428c149f449b71a0458d693b4
René Sackers <rene.sackers@live.com>
2019-11-17 23:00:41 +0100

    Added NuGet publishing tools

---
+++ .nuget/publish.bat
@@ -0,0 +1,27 @@
+@echo off
+
+dotnet publish ..\src\MindustrySchematics.Deserializer\MindustrySchematics.Deserializer.csproj -c Release /p:Version=1.2.3
+
+@echo off
+
+set version=%1
+if [%version%] neq [] goto askPublish
+set /P version="Version: "
+
+:askPublish
+
+set /P c=Are you sure you want to publish version %version%? [Y/N] 
+if /I "%c%" EQU "Y" goto :publish
+if /I "%c%" EQU "N" goto :exitCommand
+
+:publish
+dotnet publish ..\src\MindustrySchematics.Deserializer\MindustrySchematics.Deserializer.csproj -c Release /p:Version=%version%
+nuget push ..\src\MindustrySchematics.Deserializer\bin\Release\MindustrySchematics.Deserializer.%version%.nupkg -Source GitHub
+
+pause
+exit
+
+:exitCommand
+echo publish canceled
+
+pause
